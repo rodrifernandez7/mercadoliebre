dd55f988ba8042dd3eb09fdfe27ed87c27d24b0b
rodrifernandez7 <fernandezmrodrigo7@gmail.com>
2021-06-03 20:15:55 -0300

    mercadoliebre

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,3 +1,5 @@
+'use-strict'
+
 const fs = require('fs');
 const path = require('path');
 
@@ -7,61 +9,66 @@ const productsController = {
 
     filename: productsFilePath,
 
-    getData: function(){
+    getData(){
         return JSON.parse(fs.readFileSync(path.resolve(__dirname, '../data/productsDataBase.json'), 'utf-8'));
     },
 
-    generateId: function(){
+    generateId(){
         let allProducts = this.findAll();
-        let lastProduct = allProducts.pop();
+        let lastProduct = allProducts.pop(); /* saca el ultimo elemento del array */
         if(lastProduct){
             return lastProduct.id +1;
         }
         return 1;
     },
 
-    findAll: function(){
-        this.getData();
+    findAll() {
+        
+        return this.getData()
     },
 
-    findByPk: function(id){
+    findByPk(id){
         let allProducts = this.findAll();
         let productFound = allProducts.find(oneProduct => oneProduct.id === id);
         return productFound;
     },
 
-    findByField: function(field){
+    findByField(field){
         let allProducts = this.findAll();
         let productFound = allProducts.find(oneProduct => oneProduct[field] === text);
         return productFound;
     },
 
-    index: function(req , res){
+    index(req , res)  {
+        console.log(this);
+        let allProducts = this.findAll();
+        
+        return res.send(allProducts);
         res.render('home');
     },
 
-    create: function(req , res){
+    create(req , res){
         res.render('product-sell-form');
     },
 
-    store: function(productData){
+    store(req , res){
         let allProducts = this.findAll();
         let newProduct = {
             id: this.generateId(),
-            ...productData
+            ...req.body
         }
         allProducts.push(newProduct);
         fs.writeFileSync(this.filename, JSON.stringify(allProducts, null, 4));
         return true;
     },
 
-    delete: function (id){
+    delete (req, res){
         let allProducts = this.findAll();
-        let finalProducts = allProducts.filter(oneProduct => oneProduct.id !== id);
+        let finalProducts = allProducts.filter(oneProduct => oneProduct.id !== req.params.id);
         fs.writeFileSync(this.filename, JSON.stringify(finalProducts, null, 4));
         return true;
     }
 }
-/* console.log(productsController.store()); */
+console.log(productsController.findAll());
 
 module.exports = productsController;
